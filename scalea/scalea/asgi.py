e5daca72c010de8b822a10bbef0be13f3c00949c
Taras Yaremko <tarasyaremko@MacBook-Air-Taras.local>
2026-04-19 21:41:05 +0300

    Django +  DRF

---
+++ scalea/scalea/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for scalea project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scalea.settings')
+
+application = get_asgi_application()
