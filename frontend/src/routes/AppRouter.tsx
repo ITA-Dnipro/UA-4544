f5dc2d31028e7cbd07fdf408d8f02f3a4721282a
dianajnxv <dianavorona55@gmail.com>
2026-04-29 00:45:30 +0300

    solved merge conflicts

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,23 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from '../pages/Home'
-import Login from '../pages/Login'
-import Register from '../pages/Register'
-import StartupView from '../pages/StartupView'
-import InvestorDashboard from '../pages/InvestorDashboard'
-import Inbox from '../pages/Inbox'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+//import Login from "../pages/Login/Login";
+import Register from "../pages/Register";
+import StartupView from "../pages/StartupView";
+import InvestorDashboard from "../pages/InvestorDashboard";
+import Inbox from "../pages/Inbox";
+import PasswordResetRequest from "../pages/PasswordReset/PasswordResetRequest";
+import PasswordResetConfirm from "../pages/PasswordReset/PasswordResetConfirm";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
         <Route path="/register" element={<Register />} />
         <Route path="/startups/:id" element={<StartupView />} />
         <Route path="/dashboard" element={<InvestorDashboard />} />
         <Route path="/messages" element={<Inbox />} />
+        <Route path="/password-reset" element={<PasswordResetRequest />} />
+        <Route path="/reset-password" element={<PasswordResetConfirm />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
-
