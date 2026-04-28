f5e90045eefd9604211618014998e7053e92bae1
Oleksandr Hirnyi <hirnyilesyk@gmail.com>
2026-04-28 12:34:57 +0200

    changing structure

--- scalea/users/urls.py
+++ scalea/users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from users.views.auth import PasswordResetConfirmView, PasswordResetRequestView
+from users.views import PasswordResetConfirmView, PasswordResetRequestView
 
 urlpatterns = [
-    path('auth/password-reset/', PasswordResetRequestView.as_view()),
-    path('auth/password-reset/confirm/', PasswordResetConfirmView.as_view()),
+    path("auth/password-reset/", PasswordResetRequestView.as_view()),
+    path("auth/password-reset/confirm/", PasswordResetConfirmView.as_view()),
 ]
