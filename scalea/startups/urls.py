77ff32bbd3e1c2bc913ecdd4f5d5a96f2e2d7af4
ivan-kazantsev <ivan.kazantsev@proton.me>
2026-05-04 16:06:55 +0200

    fix: ruff

--- scalea/startups/urls.py
+++ scalea/startups/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from startups.views import StartupPublicProfileView
 
 urlpatterns = [
-    path('<int:pk>/', StartupPublicProfileView.as_view(), name='startup-public-profile'),
+    path(
+        '<int:pk>/', StartupPublicProfileView.as_view(), name='startup-public-profile'
+    ),
 ]
